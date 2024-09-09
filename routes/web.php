ee8ea6c94a42245402e6b133e3f37efa661c0659
daviduartedev <daviduartedev@gmail.com>
2024-09-09 17:40:12 -0300

    Adding functions to delete, and go to the saved point, functions and params

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [App\Http\Controllers\PointsController::class, 'index']); 
 Route::post('/create-point', [App\Http\Controllers\PointsController::class, 'store'])->name('points.store'); 
 Route::delete('/delete-point/{id}', [App\Http\Controllers\PointsController::class, 'destroy'])->name('points.destroy'); 
-Route::get('/buscar-dados', function () { $points = App\Models\Point::all(); return response()->json($points);});
-
-
+Route::get('/buscar-dados', function () { 
+    $points = App\Models\Point::all(); 
+    return response()->json($points);
+});
